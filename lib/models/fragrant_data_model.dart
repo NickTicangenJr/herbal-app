87dfa7fac1c79cdf8c244e462a0805ed5e3e223f
NickTicangenJr <nickkillip11@gmail.com>
2022-03-21 16:29:28 +0800

    updated

---
+++ lib/models/fragrant_data_model.dart
@@ -0,0 +1,25 @@
+class Fragrant {
+  String imageUrl;
+  String name;
+  String sname;
+
+  Fragrant({
+    required this.imageUrl,
+    required this.name,
+    required this.sname,
+  });
+}
+
+// This is the data about activity ==== This is the data about activity ====
+List<Fragrant> fragrants = [
+  Fragrant(
+    imageUrl: 'assets/images/fragrant.jpg',
+    name: 'fragrant',
+    sname: 'Aloe barbadensis miller',
+  ),
+  Fragrant(
+    imageUrl: 'assets/images/fragrant.jpg',
+    name: 'fragrant',
+    sname: 'Mimosa pudica Linn.',
+  ),
+];
